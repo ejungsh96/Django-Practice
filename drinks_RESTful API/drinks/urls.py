fdf71ec63e3a05f4d7eec728b901c2b066d1eaa7
unknown <ejungsh96@gmail.com>
2022-06-14 19:22:32 +0100

    Docker Practice

--- drinks_RESTful API/drinks/urls.py
+++ drinks_RESTful API/drinks/urls.py
@@ -13,15 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from ast import If
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drinks import views
 from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('drinks/', views.drink_list),
-    path('drinks/<int:id>', views.drink_detail)
+    path('drinks/<int:id>', views.drink_detail),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
